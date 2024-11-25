ebf854dcdd0e9b95914846b68816fae44b51e461
Skzter <v.goetze03@gmail.com>
2024-11-25 13:53:31 +0100

    changes

--- Uebung2/LSG/updateWebsite.sh
+++ Uebung2/LSG/updateWebsite.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-cd /home/vgoetze1/MMWP-Uebungen/Uebung2/LSG
+cd $HOME/MMWP-Uebungen/Uebung2/LSG
 
 cp -v "index.html" /home/vgoetze1/public_html/Beleg2MMWP
 cp -v "index.css" /home/vgoetze1/public_html/Beleg2MMWP
