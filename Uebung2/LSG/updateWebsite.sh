d8d31cd89c0192aa7b04f60bdf0a40f911357404
Skzter <v.goetze03@gmail.com>
2024-11-29 16:37:29 +0100

    script verändert

--- Uebung2/LSG/updateWebsite.sh
+++ Uebung2/LSG/updateWebsite.sh
@@ -2,6 +2,6 @@
 
 cd $HOME/MMWP-Uebungen/Uebung2/LSG
 
-cp -v "index.html" /home/vgoetze1/public_html/Beleg2MMWP
-cp -v "index.css" /home/vgoetze1/public_html/Beleg2MMWP
+cp -v "index.html" /home/vgoetze1/public_html/Portfolio
+cp -v "index.css" /home/vgoetze1/public_html/Portfolio
 
