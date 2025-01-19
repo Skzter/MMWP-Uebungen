927ee0396bf52e77cee24600881b6cf332fdc9f0
Skzter <v.goetze03@gmail.com>
2025-01-19 15:37:08 +0100

    1./2. aufg abgeschlossen

---
+++ Uebung4/LSG/SeminarAufgaben/Serie05/LSG/index.js
@@ -0,0 +1,10 @@
+let c = document.getElementById('mC');
+var ctx = c.getContext("2d");
+ctx.fillStyle = "pink";
+ctx.fillRect(0, 0, 300, 100);
+
+
+let c2 = document.getElementById('mC2');
+var ctx2 = c2.getContext("2d");
+ctx2.font = "48px serif";
+ctx2.fillText("Hello world", 50, 90);
