3603d6bd9dd78814fcdb45141847f34be1c8c1b5
Skzter <v.goetze03@gmail.com>
2025-01-20 11:08:44 +0100

    3. angefangen

--- Uebung4/LSG/SeminarAufgaben/Serie05/LSG/index.js
+++ Uebung4/LSG/SeminarAufgaben/Serie05/LSG/index.js
@@ -8,3 +8,11 @@ let c2 = document.getElementById('mC2');
 var ctx2 = c2.getContext("2d");
 ctx2.font = "48px serif";
 ctx2.fillText("Hello world", 50, 90);
+
+function setCanvas2D()
+{
+    let c3 = document.getElementById('mC3');
+    var ctx3 = c3.getContext('2d');
+}
+
+setCanvas2D();
