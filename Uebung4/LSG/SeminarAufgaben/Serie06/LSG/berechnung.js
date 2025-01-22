5b89b64bd085958a47de2005b9d2e32c3032277f
Skzter <v.goetze03@gmail.com>
2025-01-22 11:50:39 +0100

    es workt

--- Uebung4/LSG/SeminarAufgaben/Serie06/LSG/berechnung.js
+++
@@ -1,9 +0,0 @@
-self.onmessage = function(messageEvent) {
-    if (messageEvent.data == "Run") {
-        let i, summe = 0;
-        for (i = 0; i < 9000000; i++) {
-            summe += i * i + Math.random();
-        }
-        self.postMessage(summe);
-    }
-};
--- Uebung4/LSG/SeminarAufgaben/Serie06/LSG/index.js
+++ Uebung4/LSG/SeminarAufgaben/Serie06/LSG/index.js
@@ -1,4 +1,12 @@
-const workerScript = "console.log('test')"; 
-const blob = new Blob([workerScript], { type: 'application/javascript' });
-const workerUrl = URL.createObjectURL(blob);
-const worker = new Worker(workerUrl);
+let workerSource = document.querySelector("script#worker");
+let workerBlob = new Blob([workerSource.text], { type: "application/javascript" });
+let workerUrl = URL.createObjectURL(workerBlob);
+let worker = new Worker(workerUrl);
+URL.revokeObjectURL(workerUrl)
+worker.addEventListener("message", function(messageEvent)
+    {
+        console.log("Der Wörker sagt: " + messageEvent.data);
+        document.body.style.backgroundColor = "blue";
+    });
+
+
