c43183ebb665c4fde894b84c5af28bfb4f08b866
Vincent <147661349+Skzter@users.noreply.github.com>
2025-02-03 15:14:43 +0100

    js validation

--- Uebung4/LSG/SeminarAufgaben/Serie08/LSG/index.js
+++ Uebung4/LSG/SeminarAufgaben/Serie08/LSG/index.js
@@ -1,11 +1,66 @@
-function validate()
+var entries = [];
+function doStuff(event)
 {
+    event.preventDefault();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let form = document.getElementById("form");
+    let name = document.forms["form"]["name"].value;
+    let email = document.forms["form"]["email"].value;
     let pw = document.forms["form"]["pw"].value;
-    let pwe = document.forms["form"][pwe].value;
+    let pwe = document.forms["form"]["pwe"].value;
+    let datum = document.forms["form"]["datum"].valueAsDate;
+    let land = document.forms["form"]["land"].value;
+    let check = document.forms["form"]["check"].checked;
+    const data = new FormData(form);
+  
 
-    if(!(pw == pwe))
+    if((name == null) || (name == ""))
     {
-        alert("Passwort stimmt nicht überein!")
+        alert("Name muss ausgefüllt sein!");
         return false;
     }
+        
+    if(!emailRegex.test(email))
+    {
+        alert("Keine gültige Email!");
+        return false;
+    }
+
+    if (pw == null || pw == "" || pwe == null || pwe == "")
+    {
+        alert("Bitte Passwort eingeben");
+        return false;
+    } else if (pw.length < 8)
+    {
+        alert("Passwort zu kurz. Mindestens 8 Zeichen!");
+        return false;
+    } else if (!(pw == pwe))
+    {
+        alert("Passwörter stimmen nicht überein");
+        return false;
+    }
+
+    if((datum == null))
+    {
+        alert("Datum muss ausgefüllt sein!");
+        return false;
+    }
+
+    if(land == "")
+    {
+        alert("Land auswählen!")
+        return false;
+    }
+    
+    if(!check)
+    {
+        alert("AGBs akzeptieren!");
+        return false;
+    }
+
+    console.log(data);
+    entries.push(data);
+    console.log(entries);
+    form.reset();
+    return true;
 }
